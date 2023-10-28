f151cc67ee02d9eb8236400e6956055a3a0ad13a
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-10-29 05:55:07 +0800

    chore: move scripts to `src` folder

--- src/bobber.gd
+++ src/bobber.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 const VELOCITY: float = -30.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
