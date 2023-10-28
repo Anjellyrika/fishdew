b42922cd287b4e7b29fc7f12f7c21a381f23520e
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-10-29 04:41:26 +0800

    feat: implement moving bobber

---
+++ bobber.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const VELOCITY: float = -30.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_pressed("up"):
+		velocity.y += VELOCITY
+
+	move_and_slide()
