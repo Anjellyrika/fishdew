70035f0651bb488a51fe874c11f2a1bbd6aa0405
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-10-29 08:40:30 +0800

    refactor: remove comments

--- src/fish.gd
+++ src/fish.gd
@@ -16,9 +16,9 @@ func is_at_target_position():
 	return target_position == global_position
 
 func get_new_target(reverse: bool):
+	move_speed = randf_range(50.0, 150.0)
 	var max_move = 500.0
 	if reverse:
-		push_warning("hit edge, going reverse")
 		if target_vector.y < 0:
 			target_vector = Vector2(0, randf_range(0, max_move))
 		else:
@@ -26,18 +26,17 @@ func get_new_target(reverse: bool):
 	else:
 		target_vector = Vector2(0, randf_range(-1*max_move, max_move))
 	target_position = start_position + target_vector
-	
-	move_speed = randf_range(50.0, 150.0)
-	
-	push_warning("next target:", target_position)
 
 func _process(delta):
+	wait()
 	global_position = global_position.move_toward(target_position, move_speed * delta)
 	if is_at_target_position() or edge_collision:
 		get_new_target(edge_collision)
 		edge_collision = false
-	# TODO: add random wait time before next movement
 
 func _on_body_entered(body):
 	if body.get_name() != "Bobber":
 		edge_collision = true
+
+func wait():
+	pass
