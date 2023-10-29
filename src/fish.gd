85f0b3fc31b7d73aba8ed9b9b9a9732c0dc7b8a9
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-10-29 18:59:03 +0800

    feat: implement random wait timer when switching fish movement states

--- src/fish.gd
+++ src/fish.gd
@@ -1,22 +1,21 @@
 extends Area2D
 
-var move_speed: float = 50.0
-
 var start_position: Vector2
 var target_position: Vector2
 var target_vector: Vector2
 
 var edge_collision: bool = false
+var moving: bool = true
+@onready var waitTimer = $WaitControl/Timer
 
 func _ready():
 	start_position = global_position
-	target_position = start_position
+	target_position = global_position
 
 func is_at_target_position():
 	return target_position == global_position
 
 func get_new_target(reverse: bool):
-	move_speed = randf_range(50.0, 150.0)
 	var max_move = 500.0
 	if reverse:
 		if target_vector.y < 0:
@@ -28,15 +27,20 @@ func get_new_target(reverse: bool):
 	target_position = start_position + target_vector
 
 func _process(delta):
-	wait()
-	global_position = global_position.move_toward(target_position, move_speed * delta)
-	if is_at_target_position() or edge_collision:
-		get_new_target(edge_collision)
-		edge_collision = false
+	if not moving:
+		wait()
+	if moving:
+		global_position = global_position.move_toward(target_position, randf_range(50.0,100.0) * delta)
+		if is_at_target_position() or edge_collision:
+			get_new_target(edge_collision)
+			edge_collision = false
+			wait()
 
 func _on_body_entered(body):
 	if body.get_name() != "Bobber":
 		edge_collision = true
 
 func wait():
-	pass
+	if waitTimer.time_left == 0:
+		waitTimer.start(1)
+		moving = randi_range(0,1)
