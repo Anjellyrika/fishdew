c65c498f2a6f4f5761d2cfd40dc1d7d9c21a08f0
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-10-29 08:34:15 +0800

    fix: prevent fish going out of bounds

--- src/fish.gd
+++ src/fish.gd
@@ -1,31 +1,40 @@
 extends Area2D
 
-# TODO: random move speed
-@export var move_speed: float = 50.0
-var move_direction = Vector2(0, 50.0)
+var move_speed: float = 50.0
 
 var start_position: Vector2
 var target_position: Vector2
+var target_vector: Vector2
 
 var edge_collision: bool = false
 
 func _ready():
 	start_position = global_position
-	target_position = start_position + move_direction
+	target_position = start_position
 
 func is_at_target_position():
 	return target_position == global_position
 
-func get_new_target_position():
-	var target_vector = Vector2(0, randf_range(-300.0, 300.0))
+func get_new_target(reverse: bool):
+	var max_move = 500.0
+	if reverse:
+		push_warning("hit edge, going reverse")
+		if target_vector.y < 0:
+			target_vector = Vector2(0, randf_range(0, max_move))
+		else:
+			target_vector = Vector2(0, randf_range(-1*max_move, 0))
+	else:
+		target_vector = Vector2(0, randf_range(-1*max_move, max_move))
 	target_position = start_position + target_vector
+	
+	move_speed = randf_range(50.0, 150.0)
+	
+	push_warning("next target:", target_position)
 
 func _process(delta):
 	global_position = global_position.move_toward(target_position, move_speed * delta)
 	if is_at_target_position() or edge_collision:
-		get_new_target_position()
-		if global_position == start_position:
-			target_position = start_position + move_direction
+		get_new_target(edge_collision)
 		edge_collision = false
 	# TODO: add random wait time before next movement
 
