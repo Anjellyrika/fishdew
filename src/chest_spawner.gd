1eb62fb2013a674f03d7da153300c01b1e9ac2e2
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-11-01 02:31:45 +0800

    feat: spawn chest relative to collision areas

--- src/chest_spawner.gd
+++ src/chest_spawner.gd
@@ -1,10 +1,13 @@
 extends Node
 
+@onready var top_edge: Vector2 = get_parent().get_node("./FishingRod").get_child(1).get_position()
+@onready var bottom_edge: Vector2 = get_parent().get_node("./FishingRod").get_child(2).get_position()
+
 var chest_node = preload("res://src/chest.tscn")
 var isChestSpawned = false
 var rand_num: int
 
-func _process(delta):
+func _process(_delta):
 	if isChestSpawned:
 		print("chest on map")
 		pass
@@ -12,9 +15,9 @@ func _process(delta):
 func _on_timer_timeout():
 	# attempt to spawn a chest
 	if not isChestSpawned:
-		rand_num = randi_range(1,4)
+		rand_num = randi_range(1,1)
 		if rand_num == 1:
 			var chest = chest_node.instantiate()
-			chest.position = Vector2(randi_range(100,500), randi_range(100,500))
+			chest.position = Vector2(top_edge.x, randi_range(100,500))
 			add_child(chest)
 			isChestSpawned = true
