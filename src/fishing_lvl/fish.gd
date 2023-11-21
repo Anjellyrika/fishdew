3daea160d9ddd0257d2ad839e7b80e0eeb6a5ced
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-11-21 22:47:22 +0800

    feat: set random start position for fish

--- src/fishing_lvl/fish.gd
+++ src/fishing_lvl/fish.gd
@@ -7,7 +7,10 @@ signal fish_progress_decreased
 
 @export var speed_bounds: Array
 
-var start_position: Vector2
+var top_bounds: float
+var bot_bounds: float
+var rod_x: float
+#var start_position: Vector2
 var target_position: Vector2
 var target_vector: Vector2
 var edge_collision: bool = false
@@ -17,11 +20,20 @@ var moving: bool = true
 @onready var waitTimer = $WaitControl/Timer
 
 func _ready():
-	start_position = global_position
 	target_position = global_position
+	get_owner().ready.connect(set_start_position)
+
+
+func set_start_position():
+	top_bounds = root.rod_top_pos.y + root.edge_width
+	bot_bounds = root.rod_bot_pos.y - root.edge_width
+	rod_x = root.rod_top_pos.x
+	self.position = Vector2(rod_x, randf_range(top_bounds, bot_bounds))
+
 
 func is_at_target_position():
 	return target_position == global_position
+
 
 func get_new_target(reverse: bool):
 	var max_move = 500.0
@@ -32,7 +44,8 @@ func get_new_target(reverse: bool):
 			target_vector = Vector2(0, randf_range(-1*max_move, 0))
 	else:
 		target_vector = Vector2(0, randf_range(-1*max_move, max_move))
-	target_position = start_position + target_vector
+	target_position = self.position + target_vector
+
 
 func _process(delta):
 	if not moving:
@@ -44,16 +57,19 @@ func _process(delta):
 			edge_collision = false
 			wait()
 
+
 func wait():
 	if waitTimer.time_left == 0:
 		waitTimer.start(1)
 		moving = randi_range(0,1)
+
 
 func _on_body_entered(body):
 	if body.get_name() == "Bobber":
 		fish_progress_increased.emit()
 	elif body.get_name() == "FishingRod":
 		edge_collision = true	
+
 
 func _on_body_exited(body):
 	if body.get_name() == "Bobber":
