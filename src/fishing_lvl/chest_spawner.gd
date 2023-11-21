b8bad3fc824555384c843cd557b8af5c5bd70efc
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-11-22 02:00:55 +0800

    refactor: restrict chest instantiation to spawning state

--- src/fishing_lvl/chest_spawner.gd
+++ src/fishing_lvl/chest_spawner.gd
@@ -12,21 +12,20 @@ enum {
 }
 
 var state = NonExistent
-var rand_num: int
 
 @onready var root = get_owner()
 
 func _ready():
 	pass
 
+
 func _process(_delta):
-	var chest = preload("res://src/fishing_lvl/chest.tscn").instantiate()
 	match state:
 		NonExistent:
 			pass
 		Spawning: # attempt to spawn a chest
-			rand_num = randi_range(1,8)
-			if rand_num == 1:
+			if randi_range(1,8) == 1:
+				var chest = preload("res://src/fishing_lvl/chest.tscn").instantiate()
 				chest.position = Vector2(root.rod_x, randf_range(root.top_bounds, root.bot_bounds))
 				add_child(chest)
 				state = Spawned
