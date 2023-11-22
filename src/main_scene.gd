98d1903881dde0ada14e81b75478ffcf7b6a8a34
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-11-22 16:13:53 +0800

    feat: implement switch from overworld to fishing

--- src/main_scene.gd
+++ src/main_scene.gd
@@ -13,6 +13,7 @@ func _ready():
 
 func unload_level():
 	if is_instance_valid(level_instance):
+		camera.zoom = Vector2(1.5,1.5)
 		level_instance.queue_free()
 	level_instance = null
 
@@ -20,6 +21,7 @@ func unload_level():
 func load_fish_level():
 	unload_level()
 	var level_resource = load("res://src/fishing_lvl/fishing_lvl.tscn")
+	camera.zoom = Vector2(1,1)
 	if level_resource:
 		level_instance = level_resource.instantiate()
 		add_child(level_instance)
