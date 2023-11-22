956da5ee4d2bab2ff243c94a471ccee42c73e39f
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-11-22 17:17:24 +0800

    feat: set random start position for bobber

--- src/fishing_lvl/bobber.gd
+++ src/fishing_lvl/bobber.gd
@@ -3,6 +3,19 @@ extends CharacterBody2D
 const VELOCITY: float = -30.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var top_bounds
+var bot_bounds
+
+@onready var root = get_owner()
+
+func _ready():
+	get_owner().ready.connect(set_start_position)
+
+
+func set_start_position():
+	top_bounds = root.top_bounds
+	bot_bounds = root.bot_bounds
+	position = Vector2(root.rod_x, randf_range(top_bounds, bot_bounds))
 
 func _physics_process(delta):
 	if not is_on_floor():
