ee074670e0da4f8535bd89f973ed1df11e8e4101
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-11-22 22:18:42 +0800

    refactor: implement state machine for fish movement

--- src/fishing_lvl/fish.gd
+++ src/fishing_lvl/fish.gd
@@ -2,73 +2,79 @@ class_name Fish
 
 extends Area2D
 
+enum {
+	Wait,
+	Reposition,
+	Move,
+}
+
 signal fish_progress_increased
 signal fish_progress_decreased
 
-@export var speed_bounds: Array #?
+@onready var fish_size = $FishCollision
 
+# Movement bounds
 var top_bounds: float
 var bot_bounds: float
+
+# Movement variables
+var speed_bounds
+var direction
+var velocity
 var target_position: Vector2
-var target_vector: Vector2
-var has_edge_collided: bool = false
-var moving: bool = true
+
+var state = Wait
 
 @onready var root = get_owner()
 @onready var waitTimer = $WaitControl/Timer
 
 func _ready():
-	target_position = global_position
 	get_owner().ready.connect(set_start_position)
+	target_position = global_position
 
 
 func set_start_position():
 	top_bounds = root.top_bounds
 	bot_bounds = root.bot_bounds
-	position = Vector2(root.rod_x, randf_range(top_bounds, bot_bounds))
+	global_position = Vector2(root.rod_x, randf_range(top_bounds, bot_bounds))
 
 
-func is_at_target_position():
-	return target_position == global_position
-
-
-func get_new_target(reverse_direction: bool):
-	var prev_target_vector_y = target_vector.y
-	var dist_from_top = position.y - top_bounds
-	var dist_from_bot = bot_bounds - position.y
-	var distance_array = [dist_from_top, dist_from_bot]
-	if reverse_direction:
-		if prev_target_vector_y < 0:
-			target_vector = Vector2(0, randf_range(0, dist_from_bot))
-		else:
-			target_vector = Vector2(0, randf_range(-1*dist_from_top, 0))
-	else:
-		target_vector = Vector2(0, randf_range(0, distance_array.min()))
-	target_position = position + target_vector
+func get_new_target():
+	target_position = Vector2(root.rod_x, randf_range(top_bounds, bot_bounds))
 
 
 func _process(delta):
-	if not moving:
-		wait()
-	if moving:
-		global_position = global_position.move_toward(target_position, (10*randi_range(speed_bounds[0],speed_bounds[1])) * delta)
-		if is_at_target_position() or has_edge_collided:
-			get_new_target(has_edge_collided)
-			has_edge_collided = false
+	speed_bounds = 500
+	
+	match state:
+		Wait:
 			wait()
+		Reposition:
+				get_new_target()
+				state = Wait
+		Move:
+			direction = global_position.direction_to(target_position)
+			velocity = direction.y * speed_bounds * delta
+			global_position += Vector2(0, velocity)
+			if is_at_target_position():
+				state = Reposition
 
 
 func wait():
 	if waitTimer.time_left == 0:
 		waitTimer.start(1)
-		moving = randi_range(0,1)
+		state = [0,2].pick_random()
+
+
+func is_at_target_position():
+	var tolerance = fish_size.shape.height
+	print("check tolerance ", target_position.y, abs(global_position.y - target_position.y) < tolerance)##
+	return abs(global_position.y - target_position.y) < tolerance
 
 
 func _on_body_entered(body):
 	if body.get_name() == "Bobber":
 		fish_progress_increased.emit()
-	elif body.get_name() == "FishingRod":
-		has_edge_collided = true
 
 
 func _on_body_exited(body):
