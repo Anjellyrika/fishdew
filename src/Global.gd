8c16ac5c8522fdff74f436c180d78f5b67611d74
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-12-08 20:13:15 +0800

    refactor: adjust environment scaling

--- src/Global.gd
+++ src/Global.gd
@@ -2,3 +2,9 @@ extends Node
 
 @onready var fish_caught: int = 0
 @onready var treasure_inventory: int = 0
+
+@onready var center_x: int = ProjectSettings.get_setting("display/window/size/viewport_width") / 2
+@onready var center_y: int = ProjectSettings.get_setting("display/window/size/viewport_height") / 2
+
+func _ready():
+	pass
--- src/shapes/ripple.gd
+++ src/shapes/ripple.gd
@@ -1,20 +1,22 @@
 extends Node2D
 
-var start_position = Vector2(286.5,253)
+var start_position: Vector2
+var target_position: Vector2
 var target_vector = Vector2(0,-3)
-var target_position = start_position + target_vector
 var moving = true
 var timer_interval = 1
 
 @onready var root = get_owner()
 
 func _ready():
+	start_position = Vector2(Global.center_x, Global.center_y + 360)
+	target_position = start_position + target_vector
 	position = start_position
 
 
 func _draw():
-	draw_set_transform(start_position, 0, Vector2(1.25,0.5))
-	draw_circle(Vector2.ZERO,20.0,Color("6DDFFF"))
+	draw_set_transform(Vector2.ZERO, 0, Vector2(1.5,0.5))
+	draw_circle(Vector2.ZERO,32.0,Color("6DDFFF"))
 
 
 func set_move_back():
